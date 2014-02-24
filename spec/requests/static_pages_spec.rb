f44af563b43602c55fbce68d08594b8d76ab5cd4
Dzianis Dashkevich <dskecse@gmail.com>
2014-02-25 00:22:03 +0300

    Remove duplication in specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'Static pages' do
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+
   describe 'Home page' do
     it 'has the content "Sample App"' do
       visit '/static_pages/home'
@@ -9,7 +11,7 @@ describe 'Static pages' do
 
     it 'has the right title' do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{ base_title } | Home")
     end
   end
 
@@ -21,7 +23,7 @@ describe 'Static pages' do
 
     it 'has the right title' do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{ base_title } | Help")
     end
   end
 
@@ -33,14 +35,14 @@ describe 'Static pages' do
 
     it 'has the right title' do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+      expect(page).to have_title("#{ base_title } | About")
     end
   end
 
   describe 'Contact page' do
     it 'has the right title' do
       visit '/static_pages/contact'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+      expect(page).to have_title("#{ base_title } | Contact")
     end
   end
 end
