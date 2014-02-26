67b22be638f1c467fa3fe0abe210c20c0dcefde4
Dzianis Dashkevich <dskecse@gmail.com>
2014-02-26 03:16:06 +0300

    Add Bootstrap styles

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.0.0'
 gem 'rails', '4.0.3'
 gem 'pg', '0.17.1'
 
+gem 'bootstrap-sass', '~> 2.3.2'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '~> 2.4.0'
--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Microblogging
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Precompile images for Bootstrap-Sass gem
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
