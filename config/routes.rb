990473ddb4cd666511a4a6faec99f76666a1d7eb
Dzianis Dashkevich <dskecse@gmail.com>
2014-02-26 20:16:51 +0300

    Add a stub for signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Microblogging::Application.routes.draw do
   root 'static_pages#home'
+  get '/signup',  to: 'users#new'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
