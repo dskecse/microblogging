2f2740e4381ee8c54d6b3b24015cf94ad2d1fdda
Dzianis Dashkevich <dskecse@gmail.com>
2014-03-07 02:24:33 +0300

    Add tests for error messages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,6 +26,13 @@ describe 'UserPages' do
       it 'does not create a user' do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      context 'after submission' do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     context 'with valid information' do
@@ -38,6 +45,14 @@ describe 'UserPages' do
 
       it 'creates a user' do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      context 'after saving the user' do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
