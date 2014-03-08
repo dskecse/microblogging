0ab299017ff81bafd7a86da8365330044fa36886
Dzianis Dashkevich <dskecse@gmail.com>
2014-03-08 13:26:13 +0300

    Implement sign in feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   before_save { email.downcase! }
+  before_create :create_remember_token
 
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
@@ -9,4 +10,18 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
 
   has_secure_password
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Microblogging::Application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
 
   root 'static_pages#home'
+  delete '/signout', to: 'sessions#destroy'
+  get '/signin',  to: 'sessions#new'
   get '/signup',  to: 'users#new'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
