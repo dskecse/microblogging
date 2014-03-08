371611462357703167977256e3c2fc56a6a42162
Dzianis Dashkevich <dskecse@gmail.com>
2014-03-08 13:52:48 +0300

    Implement sign out feature

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,13 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.hash(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
   def current_user=(user)
     @current_user = user
   end
