fd4ccd0caa0ab3ee82160b78c9cbb568761a6c92
Dzianis Dashkevich <dskecse@gmail.com>
2014-03-09 15:41:17 +0300

    Update valid_signin helper

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -3,7 +3,7 @@ module AuthenticationHelpers
     if options[:no_capybara]
       # Sign in when not using Capybara
       remember_token = User.new_remember_token
-      cookies.permanent[:remember_token] = remember_token
+      cookies[:remember_token] = remember_token
       user.update_attribute(:remember_token, User.hash(remember_token))
     else
       visit signin_path
