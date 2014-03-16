a4a1a8ba5867a72070d555a9c97f25983c7c8bb0
Dzianis Dashkevich <dskecse@gmail.com>
2014-03-17 02:17:36 +0300

    Add constraints on micropost content

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,5 +3,6 @@ class Micropost < ActiveRecord::Base
 
   default_scope -> { order('created_at DESC') }
 
+  validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -18,4 +18,16 @@ describe Micropost do
 
     it { should_not be_valid }
   end
+
+  describe 'with blank content' do
+    before { @micropost.content = ' ' }
+
+    it { should_not be_valid }
+  end
+
+  describe 'with content is too long' do
+    before { @micropost.content = 'a' * 141 }
+
+    it { should_not be_valid }
+  end
 end
