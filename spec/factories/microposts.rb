f7d4a6795cdb94ddd52d22b7b425cdfda7524924
Dzianis Dashkevich <dskecse@gmail.com>
2014-03-17 01:43:10 +0300

    Provide proper ordering of microposts

---
+++ spec/factories/microposts.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :micropost do
+    content 'Death Metal Rooster'
+    user
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -121,4 +121,18 @@ describe User do
 
     its(:remember_token) { should_not be_blank }
   end
+
+  describe 'micropost associations' do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it 'has microposts in the right order' do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+  end
 end
