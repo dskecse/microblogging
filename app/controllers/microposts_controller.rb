6df4dd3ac002d7e5518ea1e3153d8d3392e4736d
Dzianis Dashkevich <dskecse@gmail.com>
2014-03-17 22:37:59 +0300

    Add ability to create microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,21 @@ class MicropostsController < ApplicationController
   before_action :signed_in_user
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'Micropost was successfully created.'
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
---
+++ spec/requests/micropost_pages_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'MicropostPages' do
+  subject { page }
+
+  let(:user) { FactoryGirl.create(:user) }
+  before { valid_signin user }
+
+  describe 'micropost creation' do
+    before { visit root_path }
+
+    context 'with invalid information' do
+      it 'does not create a micropost' do
+        expect { click_button 'Post' }.not_to change(Micropost, :count)
+      end
+
+      describe 'error messages' do
+        before { click_button 'Post' }
+
+        it { should have_content('error') }
+      end
+    end
+
+    context 'with valid information' do
+      before { fill_in 'micropost_content', with: 'Death Metal Rooster' }
+
+      it 'creates a micropost' do
+        expect { click_button 'Post' }.to change(Micropost, :count).by(1)
+      end
+    end
+  end
+end
