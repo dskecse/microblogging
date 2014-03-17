cf4c1b85acb6eb66669f917570d8ea4be474d8a2
Dzianis Dashkevich <dskecse@gmail.com>
2014-03-18 01:30:00 +0300

    Add ability to delete user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_action :signed_in_user
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,11 +14,19 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find(params[:id])
+    rescue
+      redirect_to root_url
     end
 end
--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -29,4 +29,16 @@ describe 'MicropostPages' do
       end
     end
   end
+
+  describe 'micropost destruction' do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    content 'as a correct user' do
+      before { visit root_path }
+
+      it 'deletes a micropost' do
+        expect { click_link 'delete' }.to change(Micropost, :count).by(-1)
+      end
+    end
+  end
 end
