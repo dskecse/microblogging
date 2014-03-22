72e1b7e847e9eeaf7a9ac39d95f88bad5951b4c2
Dzianis Dashkevich <dskecse@gmail.com>
2014-03-22 13:01:46 +0300

    Add followers and followed users to a data seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,10 @@ users = User.all(limit: 6)
     user.microposts.create!(content: Faker::Lorem.sentence)
   end
 end
+
+users = User.all
+user  = users.first
+followed_users = users[2..50]
+followers      = users[3..40]
+followed_users.each { |followed| user.follow!(followed) }
+followers.each { |follower| follower.follow!(user) }
