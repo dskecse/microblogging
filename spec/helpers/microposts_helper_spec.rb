4ec0c7b8d9afd53a1ecd606419340b9fef26ecee
Dzianis Dashkevich <dskecse@gmail.com>
2014-12-30 19:43:49 +0300

    Fix an issue w/ html entity

--- spec/helpers/microposts_helper_spec.rb
+++ spec/helpers/microposts_helper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe MicropostsHelper do
   describe 'wrap' do
     it 'adds zero width spaces to long texts' do
-      expect(wrap('a' * 35)).to eq('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#8203;aaaaa')
+      expect(wrap('a' * 35)).to eq("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\u200baaaaa")
     end
   end
 end
